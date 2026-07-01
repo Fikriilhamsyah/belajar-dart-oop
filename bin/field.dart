db00d64bedf188f100137c182601cf37f3efc6d3
Fikriilhamsyah <fikriilhamsyah27@gmail.com>
2026-07-01 10:16:42 +0700

    Manipulasi Field

--- bin/field.dart
+++ bin/field.dart
@@ -3,3 +3,17 @@ class Person{
 	String? address;
 	final String country = "Indonesia";
 }
+
+void main(){
+  // Membuat object dari class Person
+	var person = Person();
+
+  // Manipulasi data dari object person
+	person.name = "Fikri";
+	person.address = "Bogor";
+	// person.country = "Tidak bisa diubah";
+	
+	print(person.name);
+	print(person.address);
+	print(person.country);
+}
