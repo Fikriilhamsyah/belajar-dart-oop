e493596d99a2e6e036eb240c15de44a853ec5f04
Fikriilhamsyah <fikriilhamsyah27@gmail.com>
2026-07-02 09:07:43 +0700

    Method Overriding

---
+++ bin/method_overriding.dart
@@ -0,0 +1,24 @@
+class Manager {
+  String? name;
+
+  void sayHello(String name) {
+    print("Hello, $name, I am ${this.name}");
+  }
+}
+
+// Inheritance class pewarisan dari class Manager ke class VicePresident
+class VicePresident extends Manager {
+  void sayHello(String name){
+    print("Hello, $name, I am ${this.name} (Vice President)");
+  }
+}
+
+void main() {
+  var manager = Manager();
+  manager.name = "Alice";
+  manager.sayHello("Bob");
+
+  var vp = VicePresident();
+  vp.name = "Charlie";
+  vp.sayHello("David");
+}
