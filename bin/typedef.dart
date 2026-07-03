d202806b05c22f4b365ff097fcb101c9c32cd7f1
Fikriilhamsyah <fikriilhamsyah27@gmail.com>
2026-07-03 10:05:28 +0700

    Typedef

---
+++ bin/typedef.dart
@@ -0,0 +1,12 @@
+import 'data/sum.dart';
+
+void main() {
+  var sum = Sum(10, 10);
+
+  // Typedef Class
+  var jumlah = Jumlah(12, 12);
+  var total = Total(11, 11);
+
+  print(jumlah());
+  print(total());
+}
